f92031902a940841e98c0724ed89f7e3d8220bca
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-04-26 23:59:35 +0600

    load env and export as config

---
+++ backend/config/index.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+
+export { host, port, user, password, database };
