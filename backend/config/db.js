0591eb1ed770052d84faa2ef5028ae80b3b0f751
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-04-26 23:59:03 +0600

    add default export pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,24 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import { host, port, user, password, database } from './index.js';
 
 const MAX_RETRIES = 10;
 const RETRY_DELAY = 2000; // in ms
 
+const pool = new Pool({
+  host,
+  port,
+  user,
+  password,
+  database,
+});
+
 const connectWithRetry = async () => {
   for (let i = 0; i < MAX_RETRIES; i++) {
     try {
-      const pool = new Pool({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-      });
-
       await pool.query('SELECT 1'); // test query
       console.log('✅ Connected to Postgres');
-      return pool;
+      break; // exit loop if successful
     } catch (err) {
       console.log(`❌ DB connection failed (attempt ${i + 1}/${MAX_RETRIES})`);
       console.error(err.message);
@@ -32,4 +33,5 @@ const connectWithRetry = async () => {
   }
 };
 
+export default pool;
 export { connectWithRetry as connectDB };
