94de5c1f45cddb0e5f9b67c5e40fe4bd15d7d8e2
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-04-28 23:06:27 +0600

    add proxy for api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Does not affect after deployment
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
