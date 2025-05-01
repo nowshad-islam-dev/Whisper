9254e78890c2c39993e949cc6ed73a04739accb7
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-05-01 12:04:11 +0600

    update route by including middleware

--- backend/routes/conversation.route.js
+++ backend/routes/conversation.route.js
@@ -3,11 +3,12 @@ import {
   createConversation,
   getAllConversations,
 } from '../controllers/conversation.controller.js';
+import { authenticateToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/', createConversation);
 
-router.get('/:userId', getAllConversations);
+router.get('/:userId', authenticateToken, getAllConversations);
 
 export default router;
