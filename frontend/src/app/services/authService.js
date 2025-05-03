8e0c0028cee1331e6c832291ba92c0970eaa8a9c
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-05-03 15:17:12 +0600

    fix response structure

--- frontend/src/app/services/authService.js
+++ frontend/src/app/services/authService.js
@@ -1,16 +1,18 @@
 import apiClient from './axiosClient.js';
 
 export const register = async (userData) => {
-  const response = await apiClient.post('/register', userData);
-  return response.data;
+  const response = await apiClient.post('/auth/register', userData);
+  return response.data.data;
 };
 
 export const login = async (credentials) => {
-  const response = await apiClient.post('/login', credentials);
-  return response.data;
+  const response = await apiClient.post('/auth/login', credentials);
+  return response.data.data;
 };
 
 export const refreshAccessToken = async (refreshToken) => {
-  const response = await apiClient.post('/refresh-token', { refreshToken });
-  return response.data;
+  const response = await apiClient.post('/auth/refresh-token', {
+    refreshToken,
+  });
+  return response.data.data;
 };
